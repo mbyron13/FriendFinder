113d298f9cdc2539b558aa0165b87ccce1c6889b
Mike B <michael.byron@gmail.com>
2019-05-03 15:40:21 -0400

    functional

--- app/public/js/friendfinder.js
+++ app/public/js/friendfinder.js
@@ -1,5 +1,34 @@
-var dataFriend = require("../../data/friends");
 
-$(document).on("click","#submit",function (){
+$(document).on("click", "#submit", function () {
+    var person = {};
+
+    person.name = $("#name").val();
+    person.photo = $("#photo").val();
+    person.scores = [];
+    var selectEles = $(".select");
+
+    for (var i = 0; i < selectEles.length; i++) {
+        person.scores.push($(selectEles[i]).val());
+    }
+    console.log(person);
+
+    $.ajax({
+        url: "/api/friends",
+        method: "POST",
+        data: JSON.stringify(person),
+        contentType: "application/json"
+    }).done(function (msg) {
+        console.log(msg);
+        displayNewBestFriend(msg);
+    }).fail(function(jqXHR){
+        console.log(jqXHR.responseText)
+    });
+
 
 });
+
+function displayNewBestFriend(person){
+    $("#matchname").html(person.name);
+    $("#matchpic").attr("src", person.photo);
+    $("#friendsModal").modal("show");
+}
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -15,13 +15,33 @@ router.post("/friends", function (req, res) {
 
     fs.readFile(path.join(__dirname, "../data/friends.json"), function (err, data) {
         var people = JSON.parse(data);
-
+        var newBestFriend = calcNewBestFriend(people, newPerson)
         people.push(newPerson);
-        
+
         fs.writeFile(path.join(__dirname, "../data/friends.json"), JSON.stringify(people), { encoding: "utf-8" }, function (err) {
             if (err) throw err;
-            res.sendStatus(202);
+            res.status(202).send(newBestFriend);
         });
     });
 });
+
+function calcNewBestFriend(people, newPerson) {
+    var newBestFriendIndex = 0;
+    var newBestFriendDiff = 0;
+
+    for (var i = 0; i < people.length; i++) {
+        var diff = 0;
+        for (var j = 0; j < newPerson.scores.length; j++){
+           diff += Math.abs(newPerson.scores[j] - people[i].scores[j]);
+        }
+        if(i == 0 || diff < newBestFriendDiff){
+            newBestFriendIndex = i;
+            newBestFriendDiff = diff;
+        }
+    }
+    console.log(people[newBestFriendIndex]);
+    console.log("people new best friend index: " + people[newBestFriendIndex]);
+    console.log("new best friend index: "+ newBestFriendIndex);
+    return people[newBestFriendIndex];
+}
 module.exports = router;
